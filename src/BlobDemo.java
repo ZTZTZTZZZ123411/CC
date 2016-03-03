9714499daec51b3cf2b79c88257ee488226336cc
TaekEun <goodfax2000@naver.com>
2016-03-03 12:38:22 +0900

    test2

--- src/BlobDemo.java
+++ src/BlobDemo.java
@@ -14,7 +14,7 @@ import static org.bytedeco.javacpp.opencv_imgproc.*;
 //*             - Dave Grossman                                 *//
 //*                                                             *//
 ///////////////////////////////////////////////////////////////////
-///수정해보기!
+///update2
 public class BlobDemo
 {
     public static void main(String[] args)
